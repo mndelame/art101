3e6c186557441f29d65970c941c25639c01a7ade
mndelame <mgdela29@gmail.com>
2024-10-16 18:35:23 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,37 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock, Paper, Scissors
+//An implementation of the game
+// Author: Madison Grace De La Merced
+// Date: October 16th
+
+//Pseudocode for a Rock, Paper, Scissor game
+
+Start
+    //2 players (player 1 = "P1", player 2 = "P2")
+      //prompt users for a choice ("rock", "paper", or "scissors")
+      //If P1 choses rock and P2 chooses scissors,
+        //Print P1 rock wins
+      //If P2 chooses rock and P1 chooses scissors,
+        //Print P2 rock wins
+      //If P1 player choses rock and P2 choses paper,
+        //Print P2 paper wins
+      //If P2 player choses rock and P1 chooses paper,
+        //Print P1 paper wins
+      //If P1 player chooses scissors and P2 chooses paper,
+        //Print P1 scissors wins
+      //If P2 player chooses scissors and and P1 chooses paper,
+        //Print P2 scissors wins 
+      //If P1 and P2 choose rock, repeat step 2
+        // Print Tie
+      //If P1 and P2 choose scissors, repeat step 2
+        //Print Tie
+      //If P1 and P2 choose paper, repeat step 2
+        //Print Tie      
+End 
+    
+
+
+
+
 
 // Constants
 
